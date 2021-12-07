aa0d97c36e40ea63bed6b24860e236d18099770d
vici77 <95707427+vici77@users.noreply.github.com>
2021-12-08 00:13:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Angel",
-    introTitle: 'AnhPhuong',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    introTitle: 'Bảo Ngọc',
+    introDesc: `Nói chiện mầy hài vl`,
+    btnIntro: '^^Oki^^',
+    title: 'Chúa hề',
+    desc: 'Ngọc trả tiền ăn nhẩy ',
+    btnYes: 'Đồng ý <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Đi ăn buffet nhá',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'Đi thì đi mà không đi thì đi <33333333',
+    mess: 'Mình biết mà. Yêu bạn',
+    messDesc: 'Hôm sau mầy qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/messages/t/100005723373827' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
